a5018370306ce1d7fcc0715ac9b8125413d85de2
Sid <sidharth.bairy@gmail.com>
2026-01-18 06:01:24 +0800

    Removed salt/updated becrypt

--- backend/app.py
+++ backend/app.py
@@ -17,9 +17,6 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# bcrypt config
-SALT = os.getenv("SALT").encode('utf-8')
-
 # Supabase configuration
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
