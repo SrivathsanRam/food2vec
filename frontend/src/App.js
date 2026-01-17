d708ecd400348c060b10772d86029bdf7f3cdcc3
Unknownflow <clarencelau88@gmail.com>
2026-01-18 05:42:23 +0800

    feat: add logout button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Navbar />
+
                 <AppRoutes />
             </BrowserRouter>
         </div>
